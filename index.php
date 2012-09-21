6114c3541a79e85b4bd194479113c6c58aa735c4
Anders Tolborg <anders@tolborg.dk>
2012-09-21 11:47:30 +0200

    Add index.php file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width">
+
+  <title>BIBLIOTEK.DK</title>
+  <meta name="description" content="">
+
+</head>
+
+<body>
+
+	<ul>
+		<li><a href="pages/front.php">Forside</a></li>
+		<li><a href="pages/search-result.php">Søgeresultat (værkvisning)</a></li>
+		<li><a href="pages/laanerstatus.php">Lånerstatus</a></li>
+	</ul>
+
+
+</body>
+
