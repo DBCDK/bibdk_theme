c562a1eca6c15a59e012a8ca7c8646cf9b35412f
pjohans <pjohans@gmail.com>
2012-10-01 16:35:12 +0200

    theming bibdk_helpdesk

---
+++ templates/blocks/bibdk-helpdesk-block.tpl.php
@@ -0,0 +1,18 @@
+<div class="grid_5">
+ <div class="widget widget-biblioteksvagt widget-height-2">
+  <div class="inner">
+<div class="field-title">
+   <h3>
+   <?php echo $title;?>
+</h3>
+</div>
+<img src="<?php echo $image; ?>" style="width:100px"/>
+<div class="clearfix"></div>
+   <?php echo $description;?>
+   <footer>
+   <?php echo $link;?>
+   </footer>
+  </div>
+ </div>
+</div>
+
