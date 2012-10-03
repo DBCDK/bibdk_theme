406793f18d13b7d186824eacd20ba455346c7bc1
Anders Tolborg <anders@tolborg.dk>
2012-10-03 15:18:09 +0200

    Implement Twitter Bootstrap grid system (make it easier to nest columns)

--- page.tpl.php
+++ page.tpl.php
@@ -1,9 +1,11 @@
 <?php if (!empty($page['topbar'])): ?>
   <div id="topbar-wrapper">
-    <div class="container_24">
-      <div class="grid_24">
-        <?php print render($page['topbar']); ?>
-      </div>
+    <div class="container">
+    	<div class="row">
+	      <div class="span24">
+	        <?php print render($page['topbar']); ?>
+	      </div>
+   		</div>
     </div>
   </div>
 <?php endif; ?>
@@ -11,17 +13,19 @@
 
 
   <div id="header-wrapper">
-    <div class="container_24">
-      <div class="grid_24">
-        <div id="header-logo">
-          <a href="<?php print $base_path; ?>">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Bibliotek.dk logo'); ?>"/>
-          </a>
-        </div>
-        <?php if (!empty($page['header'])): ?>
-          <?php print render($page['header']); ?>
-        <?php endif; ?>
-      </div>
+    <div class="container">
+    	<div class="row">
+	      <div class="span24">
+	        <div id="header-logo">
+	          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>">
+	            <img src="<?php print $logo; ?>" alt="<?php print t('Bibliotek.dk logo'); ?>"/>
+	          </a>
+	        </div>
+	        <?php if (!empty($page['header'])): ?>
+	          <?php print render($page['header']); ?>
+	        <?php endif; ?>
+	      </div>
+    	</div>
     </div>
   </div>
 <!-- #header-wrapper -->
@@ -29,10 +33,12 @@
 
 <?php if (!empty($page['search_panel'])): ?>
   <div id="search-panel-wrapper">
-    <div class="container_24">
-      <div class="grid_24">
-        <?php print render($page['search_panel']); ?>
-      </div>
+    <div class="container">
+    	<div class="row">
+	      <div class="span24">
+	        <?php print render($page['search_panel']); ?>
+	      </div>
+	    </div>
     </div>
   </div>
 <?php endif; ?>
@@ -41,10 +47,12 @@
 
 <?php if (!empty($messages)): ?>
   <div id="messages">
-    <div class="container_24">
-      <div class="grid_24">
-        <?php print $messages; ?>
-      </div>
+    <div class="container">
+    	<div class="row">
+	      <div class="span24">
+	        <?php print $messages; ?>
+	      </div>
+	    </div>
     </div>
   </div>
 <?php endif; ?>
@@ -53,6 +61,10 @@
 <div id="subjects-wrapper">
 
 </div>
+
+
+<!-- PANELS BELOW THIS -->
+
 
 <?php if (!empty($page['information'])): ?>
   <div id="information-wrapper">
@@ -63,31 +75,36 @@
     <!-- #information-wrapper -->
 <?php endif; ?>
 
+
 <?php if (!empty($page['content'])): ?>
 <div id="page-columns">
-  <div class="container_24">
-    <div class="grid_24">
-<?php print render( $tabs); ?>
-      <?php print render($page['content']); ?>
-    </div>
+  <div class="container">
+  	<div class="row">
+	    <div class="span24">
+				<?php print render( $tabs); ?><!-- ##FIX"" -->
+	      <?php print render($page['content']); ?>
+	    </div>
+  	</div>
   </div>
 </div>
 <?php endif; ?>
 <!-- #page-columns -->
 
-<?php
-$footer_logo = theme_get_setting('bibdk_theme_footer_logo');
-if (!empty($footer_logo)) :
-  $footer_logo = file_create_url(drupal_get_path('theme', 'bibdk_theme') . '/' . $footer_logo);
-?>
+
+<!-- PANELS ABOVE THIS -->
+
+
+
+
 <div id="footer-wrapper">
-  <div class="container_24">
-    <div class="grid_24">
-      <div id="footer-logo">
-        <img src="<?php print $footer_logo; ?>" alt="<?php print t('Bibliotek.dk - loan of books, music, and films'); ?>" />
-      </div>
+  <div class="container">
+  	<div class="row">
+	    <div class="span24">
+	      <div id="footer-logo">
+	        <img src="<?php print $footer_logo; ?>" alt="<?php print t('Bibliotek.dk - loan of books, music, and films'); ?>" />
+      	</div>
+    	</div>
     </div>
   </div>
 </div>
-<?php endif; ?>
 <!-- #footer-wrapper -->
