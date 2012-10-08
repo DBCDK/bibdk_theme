22752c213c66f012b88470977a7b74740b823ceb
Jorgen G. Nielsen <jgn@dbc.dk>
2012-10-08 16:15:56 +0200

    added styling for facets

--- layouts/bibdk_twocolumn/panels-bibdk-twocolumn.tpl.php
+++ layouts/bibdk_twocolumn/panels-bibdk-twocolumn.tpl.php
@@ -5,12 +5,14 @@
  */
 ?>
 
-<div class="panel-display panel-2col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel panel-col-first">
+<div class="panel-display panel-2col container_24 clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+
+  <div class="panel-panel panel-col-first grid_5">
     <div class="inside"><?php print $content['left']; ?></div>
   </div>
 
-  <div class="panel-panel panel-col-last">
+  <div class="panel-panel panel-col-last grid_19">
     <div class="inside"><?php print $content['middle']; ?></div>
   </div>
+
 </div>
