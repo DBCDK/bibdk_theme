581c18c0fc9c86daebfd9e9a782cf43c89d96653
martin møller <mmj@dbc.dk>
2012-10-09 17:27:40 +0200

    Show more info

--- templates/views/ting_openformat_more_info.tpl.php
+++ templates/views/ting_openformat_more_info.tpl.php
@@ -1,3 +1,3 @@
-<a href="#" class="">
-  <span class="icon icon-left icon-blue-plus">▼</span><strong>ASDASDSAD</strong> <?php print drupal_render($showinfoform); ?>
-</a>
+<div class="toggle-next-section toggle-work">
+  <?php print $showinfoform; ?>
+</div>
