73c6b02119000abaf07b310f9010520dced0d843
martin møller <mmj@dbc.dk>
2012-10-10 16:40:09 +0200

    Work abstract added

--- templates/views/ting_openformat_work.tpl.php
+++ templates/views/ting_openformat_work.tpl.php
@@ -1,112 +1,102 @@
-<?php print drupal_render($variables['fields']); ?>
-
-
-
+<?php #print drupal_render($variables['fields']); ?>
+<!-- cover -->
 <div class="work-shared-data clearfix">
-        <div class="work-cover">
-          <div class="work-cover-image">
-            <a href="#"><img src="../img/cover-front-large.gif"></a>
-            <a href="#" class="visuallyhidden"><img src="../img/cover-back-large.gif"></a>
-          </div>
-          <div class="work-cover-selector clearfix">
-            <a href="#" class="work-cover-front active"></a>
-            <a href="#" class="work-cover-back"></a>
-          </div>
+  <div class="work-cover">
+    <div class="work-cover-image">
+      <a href="#"><img src="../img/cover-front-large.gif"></a>
+      <a href="#" class="visuallyhidden"><img src="../img/cover-back-large.gif"></a>
+    </div>
+    <div class="work-cover-selector clearfix">
+      <a href="#" class="work-cover-front active"></a>
+      <a href="#" class="work-cover-back"></a>
+    </div>
 
-        </div>
-        <!-- cover -->
-
-
-        <div class="wrapper">
-
-          <div class="field-summary text">
-            <p>
-              <strong>Krimi.</strong> Et bankrøveri i Oslo udvikler sig til en ren 
-              henrettelse. Kriminalassistent Harry Hole er muligvis den sidste, 
-              der har set sin gamle kæreste inden hun bliver myrdet, og sammenhængen 
-              mellem de to forbrydelser fører til en sigøjner i en fængselscelle.
-            </p>
-          </div>
-
-          <div class="field-tags">
-            <a href="#">Krimi</a>&nbsp;/&nbsp;<a href="#">romaner</a>&nbsp;/&nbsp;<a href="#">hævn</a>&nbsp;/&nbsp;<a href="#">Oslo</a>&nbsp;/&nbsp;<a href="#">Norge</a>&nbsp;/&nbsp;<a href="#">2000-2009</a>					</div>
+  </div>
+  <!-- cover -->
 
 
-          <div class="tabs tabs-light">
-            <div class="tabs-nav clearfix">
-              <a href="#" class="active">Find mere om</a>
-              <a href="#">Anmeldelser</a>
-            </div>
-            <!-- tabs-nav -->
-
-            <div class="tabs-sections">
-              <div class="tabs-section">
-                <div class="padded text clearfix">
-                  <ul>
-                    <li><a href="#">Se anmeldelse hos Infomedia</a></li>
-                    <li><a href="#">Se materialevurdering</a></li>
-                    <li><a href="#">Læs brugeranmeldelser og ratings</a></li>
-                  </ul>
-                </div>
-              </div>
-              <!-- tabs-section -->
-            </div>
-            <!-- tabs-sections -->
-          </div>
-          <!-- tabs -->
-        </div>
-        <!-- wrapper -->
+  <div class="wrapper">
+    <div class="field-summary text">
+      <p>
+        <?php print drupal_render($fields['ting_openformat_work_abstract']); ?>
+      </p>
+    </div>
+    <div class="field-tags">
+      <a href="#">Krimi</a>&nbsp;/&nbsp;<a href="#">romaner</a>&nbsp;/&nbsp;<a href="#">hævn</a>&nbsp;/&nbsp;<a href="#">Oslo</a>&nbsp;/&nbsp;<a href="#">Norge</a>&nbsp;/&nbsp;<a href="#">2000-2009</a></div>
+    <div class="tabs tabs-light">
+      <div class="tabs-nav clearfix">
+        <a href="#" class="active">Find mere om</a>
+        <a href="#">Anmeldelser</a>
       </div>
-      <!-- work-share-data -->
- 
-      <div class="tabs tabs-heavy">
-             <?php print $variables['tabs']; ?>
-        <!-- tabs-nav -->
-        <div class="tabs-sections">
-          <div class="tabs-section">
-            <div class="padded text clearfix">
-              <div class="actions">
-                <div class="primary-actions">
-                  <div class="btn-wrapper">
-                    <a class="btn btn-blue" href="#">Bestil <strong>bog</strong> uanset udgave</a>
-                  </div>
-                  <a class="text-small link-add-basket" href="#">
-                    <span class="icon icon-left icon-blue-addbasket">▼</span>Tilføj indkøbskurv
-                  </a>
-                </div>
-              </div>
-              <a href="#" class="text-small text-lightgrey">
-                <span class="icon icon-left icon-lightgrey-list">▼</span>Hvilke biblioteker har materialet?
-              </a>
-            </div>
-            <!-- tabs-content -->
-            <div class="manifestations zebra-wrapper">
-              <div class="zebra-toggle">
-                <a href="#">
-                  <span class="icon icon-left icon-blue-down">▼</span>Skjul alle <strong>4 bøger</strong>
-                </a>
-              </div>
-              <div class="zebra-content">
-                <?php $subwork = current($variables['subWorks']); ?>
-                <?php foreach($subwork['manifestations'] as $manifestation) : ?> 
-                  <?php print $manifestation; ?>
-                <?php endforeach; ?>
-                
-              </div>
-              <!-- end zebra-content -->  
-
-              <div class="zebra-toggle">
-                <a href="#">
-                  <span class="icon icon-left icon-blue-down">▼</span>Skjul alle <strong>4 bøger</strong>
-                </a>
-              </div>
-
-            </div>
-            <!-- manifestations -->
+      <!-- tabs-nav -->
+      <div class="tabs-sections">
+        <div class="tabs-section">
+          <div class="padded text clearfix">
+            <ul>
+              <li><a href="#">Se anmeldelse hos Infomedia</a></li>
+              <li><a href="#">Se materialevurdering</a></li>
+              <li><a href="#">Læs brugeranmeldelser og ratings</a></li>
+            </ul>
           </div>
-          <!-- tabs-section -->
         </div>
-        <!-- tabs-sections -->
+        <!-- tabs-section -->
+      </div>
+      <!-- tabs-sections -->
+    </div>
+    <!-- tabs -->
+  </div>
+  <!-- wrapper -->
+</div>
+<!-- work-share-data -->
+
+<div class="tabs tabs-heavy">
+  <?php print $variables['tabs']; ?>
+  <!-- tabs-nav -->
+  <div class="tabs-sections">
+    <div class="tabs-section">
+      <div class="padded text clearfix">
+        <div class="actions">
+          <div class="primary-actions">
+            <div class="btn-wrapper">
+              <a class="btn btn-blue" href="#">Bestil <strong>bog</strong> uanset udgave</a>
+            </div>
+            <a class="text-small link-add-basket" href="#">
+              <span class="icon icon-left icon-blue-addbasket">▼</span>Tilføj indkøbskurv
+            </a>
+          </div>
+        </div>
+        <a href="#" class="text-small text-lightgrey">
+          <span class="icon icon-left icon-lightgrey-list">▼</span>Hvilke biblioteker har materialet?
+        </a>
+      </div>
+      <!-- tabs-content -->
+      <div class="manifestations zebra-wrapper">
+        <div class="zebra-toggle">
+          <a href="#">
+            <span class="icon icon-left icon-blue-down">▼</span>Skjul alle <strong>4 bøger</strong>
+          </a>
+        </div>
+        <div class="zebra-content">
+          <?php $subwork = current($variables['subWorks']); ?>
+          <?php foreach ($subwork['manifestations'] as $manifestation) : ?> 
+            <?php print $manifestation; ?>
+          <?php endforeach; ?>
+
+        </div>
+        <!-- end zebra-content -->  
+
+        <div class="zebra-toggle">
+          <a href="#">
+            <span class="icon icon-left icon-blue-down">▼</span>Skjul alle <strong>4 bøger</strong>
+          </a>
+        </div>
 
       </div>
-      <!-- tabs -->
+      <!-- manifestations -->
+    </div>
+    <!-- tabs-section -->
+  </div>
+  <!-- tabs-sections -->
+
+</div>
+<!-- tabs -->
