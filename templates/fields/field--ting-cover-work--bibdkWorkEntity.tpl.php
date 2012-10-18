f0e3b9b9014d2e6bfd19d640a96695281b01becd
Søren Vibjerg <vibjerg@gmail.com>
2012-10-18 13:20:30 +0200

    remove tabs under cover adjust width of manifestation-data table th elements

--- templates/fields/field--ting-cover-work--bibdkWorkEntity.tpl.php
+++ templates/fields/field--ting-cover-work--bibdkWorkEntity.tpl.php
@@ -5,8 +5,8 @@
       <!-- <a href="#"><img src="../img/cover-front-large.gif"></a>
       <a href="#" class="visuallyhidden"><img src="../img/cover-back-large.gif"></a> -->
     </div>
-    <div class="work-cover-selector clearfix">
+    <!--<div class="work-cover-selector clearfix">
       <a href="#" class="work-cover-front active"></a>
       <a href="#" class="work-cover-back"></a>
-    </div>
+    </div>-->
   </div>
