b7c2c7c1b4618ec2d449625f8738384431c3c11e
martin møller <mmj@dbc.dk>
2012-10-18 09:06:34 +0200

    deleted block--system--main.tpl.php

--- templates/blocks/block--system--main.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php print render($elements['name']); ?>
-<?php print render($elements['pass']); ?>
-<?php print render($elements['persistent_login']); ?>
-<?php print render($elements['password_link']); ?>
-<br />
-<?php print render($elements['new_user_link']); ?>
