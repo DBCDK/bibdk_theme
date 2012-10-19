f29706bb77cf4929ea62ef99d4a91da592d80ff2
Søren Vibjerg <vibjerg@gmail.com>
2012-10-19 09:56:01 +0200

    lift-columns

--- page--overlay.tpl.php
+++ page--overlay.tpl.php
@@ -8,6 +8,21 @@
   </div>
 <?php endif; ?>
 
-<?php if (!empty($page['content'])): ?>        
-  <?php print render($page['content']); ?>
-<?php endif; ?>
+<section id="columns-wrapper">
+
+  <div class="container">
+    <div class="row">
+      <div class="span24">
+
+        <div id="columns">
+          <?php print render($tabs); ?>
+          <?php print render($page['content']); ?>
+        </div>
+        <!-- #columns -->
+
+      </div>
+    </div>
+  </div>
+
+</section>
+<!-- #columns-wrapper -->
