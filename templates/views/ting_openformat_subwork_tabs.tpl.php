3a41dca44031b86a3c36055dbaae49a1bc638488
martin møller <mmj@dbc.dk>
2012-10-24 15:21:32 +0200

    tabs on search results in two levels

--- templates/views/ting_openformat_subwork_tabs.tpl.php
+++ templates/views/ting_openformat_subwork_tabs.tpl.php
@@ -1,5 +1,15 @@
 <div class="tabs-nav clearfix">
-  <?php foreach ($variables['subWorks'] as $type => $subWork) : $count++; ?>
-    <a href="#<?php print $type; ?>" class="<?php print ($count == 1) ? "active" : "" ?>"><?php echo $subWork['type']; ?></a>
+  <?php $outerCount = 0; ?>
+  <?php foreach ($variables['subWorks'] as $type => $subWork) : $outerCount++?>
+    <a href="#<?php print $type; ?>" class="<?php print ($outerCount == 1) ? "active" : "" ?>"><?php  print $type; ?></a>
+  <?php endforeach; ?>
+</div>
+
+<div class="tabs-nav clearfix">
+  <?php $innerCount = 0; ?>
+  <?php foreach ($variables['subWorks'] as $type => $subWork) : ?>
+    <?php foreach ($subWork as $subtype => $manifestation) : $innerCount++; ?>
+      <a href="#<?php print $subtype; ?>" class="<?php print ($innerCount == 1) ? "active" : "" ?>"><?php print $manifestation['translation']['subtype']; ?></a>
+    <?php endforeach;?>
   <?php endforeach; ?>
 </div>
--- templates/views/ting_openformat_work.tpl.php
+++ templates/views/ting_openformat_work.tpl.php
@@ -1,5 +1,5 @@
 <!-- cover -->
-  <?php print $cover ?>
+<?php print $cover ?>
 <!-- cover -->
 
 <div class="wrapper">
@@ -15,49 +15,51 @@
   <?php print $tabs; ?>
   <!-- tabs-nav -->
   <div class="tabs-sections">
-    <?php foreach ($subWorks as $type => $subwork) : $count++; ?> 
-    
-    <div id="<?php print $type; ?>" class="subwork tabs-section <?php print ($count != 1) ? "visuallyhidden" : "" ?>">
-      <div class="padded text clearfix">
-        <div class="actions">
-          <div class="primary-actions">
-            <div class="btn-wrapper">
-              <?php echo drupal_render($subwork['fields']); ?>
-              <!--<a class="btn btn-blue" href="#">Bestil <strong>bog</strong> uanset udgave</a>-->
+    <?php $count = 0; ?>
+    <?php foreach ($subWorks as $type) : ?>
+      <?php foreach ($type as $subtype => $manifest) : $count++; ?>
+        <div id="<?php print $subtype; ?>" class="subwork tabs-section <?php print ($count != 1) ? "visuallyhidden" : ""  ?>">
+          <div class="padded text clearfix">
+            <div class="actions">
+              <div class="primary-actions">
+                <div class="btn-wrapper">
+                  <?php echo drupal_render($subwork['fields']); ?>
+                  <!--<a class="btn btn-blue" href="#">Bestil <strong>bog</strong> uanset udgave</a>-->
+                </div>
+                <a class="text-small link-add-basket" href="#">
+                  <span class="icon icon-left icon-blue-addbasket">▼</span>Tilføj indkøbskurv
+                </a>
+              </div>
             </div>
-            <a class="text-small link-add-basket" href="#">
-              <span class="icon icon-left icon-blue-addbasket">▼</span>Tilføj indkøbskurv
+            <a href="#" class="text-small text-lightgrey">
+              <span class="icon icon-left icon-lightgrey-list">▼</span>Hvilke biblioteker har materialet?
             </a>
           </div>
-        </div>
-        <a href="#" class="text-small text-lightgrey">
-          <span class="icon icon-left icon-lightgrey-list">▼</span>Hvilke biblioteker har materialet?
-        </a>
-      </div>
-      <!-- tabs-content -->
-      <div class="manifestations zebra-wrapper">
-        <div class="zebra-content">
-          <?php $count_manifestation = 0; ?>
-          <?php foreach ($subwork['manifestations'] as $manifestation) : $count_manifestation++; ?> 
-            <div class="manifestation zebra <?php print ($count_manifestation > 2) ? "visuallyhidden toggle" : "" ?>">
-            <?php print $manifestation; ?>
-            </div>  
-          <?php endforeach; ?>
+          <!-- tabs-content -->
+          <div class="manifestations zebra-wrapper">
+            <div class="zebra-content">
+              <?php $count_manifestation = 0; ?>
+              <?php foreach ($manifest['manifest']['manifestations'] as $manifestation) : $count_manifestation++; ?> 
+                <div class="manifestation zebra <?php print ($count_manifestation > 2) ? "visuallyhidden toggle" : ""  ?>">
+                  <?php print $manifestation; ?>
+                </div>  
+              <?php endforeach; ?>
 
+            </div>
+            <!-- end zebra-content -->  
+            <?php if (count($manifest['manifest']['manifestations']) > 2) : ?>
+              <div class="zebra-toggle">
+                <a href="#<?php print $subtype; ?>">
+                  <span class="icon icon-left icon-blue-down">▼</span>
+                  <span class="toggle-text"><?php print t("show all (@count)", array('@count' => count($manifest['manifest']['manifestations']))); ?></span>
+                  <span class="toggle-text hidden"><?php print t("hide"); ?></span>
+                </a>
+              </div>
+            <?php endif; ?>
+          </div>
+          <!-- manifestations -->
         </div>
-        <!-- end zebra-content -->  
-        <?php if(count($subwork['manifestations']) > 2) : ?>
-        <div class="zebra-toggle">
-          <a href="#<?php print $type; ?>">
-            <span class="icon icon-left icon-blue-down">▼</span>
-              <span class="toggle-text"><?php print t("show all (@count)", array('@count' => count($subwork['manifestations']))); ?></span>
-              <span class="toggle-text hidden"><?php print t("hide"); ?></span>
-          </a>
-        </div>
-        <?php endif; ?>
-      </div>
-      <!-- manifestations -->
-    </div>
+      <?php endforeach; ?>
     <?php endforeach; ?>
     <!-- tabs-section -->
   </div>
