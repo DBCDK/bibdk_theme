93cb88633d8056c96ba739f23a4c5b58a1d38be7
martin møller <mmj@dbc.dk>
2012-10-25 11:41:49 +0200

    Styling of sub tabs in search results

--- templates/views/ting_openformat_subwork_tabs.tpl.php
+++ templates/views/ting_openformat_subwork_tabs.tpl.php
@@ -1,15 +1,20 @@
 <div class="tabs-nav clearfix">
-  <?php $outerCount = 0; ?>
-  <?php foreach ($variables['subWorks'] as $type => $subWork) : $outerCount++?>
-    <a href="#<?php print $type; ?>" class="<?php print ($outerCount == 1) ? "active" : "" ?>"><?php  print $type; ?></a>
+  <?php $counter = 0; ?>
+  <?php foreach ($variables['subWorks'] as $type => $subWork) : $counter++ ?>
+    <a href="#<?php print $type; ?>" class="<?php print ($counter == 1) ? "active" : ""  ?>"><?php print $type; ?></a>
+    <?php foreach ($subWork as $subtype => $manifestation) : $innerCount++; ?>
+      <?php $typesarray[$type][] = array($subtype, $manifestation['translation']['subtype']); ?>
+    <?php endforeach; ?>
   <?php endforeach; ?>
 </div>
 
 <div class="tabs-nav clearfix">
-  <?php $innerCount = 0; ?>
-  <?php foreach ($variables['subWorks'] as $type => $subWork) : ?>
-    <?php foreach ($subWork as $subtype => $manifestation) : $innerCount++; ?>
-      <a href="#<?php print $subtype; ?>" class="<?php print ($innerCount == 1) ? "active" : "" ?>"><?php print $manifestation['translation']['subtype']; ?></a>
-    <?php endforeach;?>
+  <?php $counter = 0; ?>
+  <?php foreach ($typesarray as $type => $subtypes) : $counter++ ?>
+    <ul id="<?php print $type ?>" class="<?php print ($counter != 1) ? "visuallyhidden" : ""  ?>">
+      <?php foreach ($subtypes as $subtype) : ?>
+        <li><a href="#<?php print $subtype[0]; ?>"><?php print $subtype[1]; ?></a></li>
+      <?php endforeach; ?>
+    </ul>
   <?php endforeach; ?>
 </div>
