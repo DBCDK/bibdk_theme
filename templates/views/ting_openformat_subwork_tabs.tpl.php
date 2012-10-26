868912db04983ed5986a16a92d04e65723152ce1
Martin Møller <mmj@dbc.dk>
2012-10-26 11:45:21 +0200

    Added material counting on tabs

--- templates/views/ting_openformat_subwork_tabs.tpl.php
+++ templates/views/ting_openformat_subwork_tabs.tpl.php
@@ -1,10 +1,13 @@
 <div class="tabs-nav clearfix">
   <?php $counter = 0; ?>
   <?php foreach ($variables['subWorks'] as $type => $subWork) : $counter++ ?>
-    <a href="#<?php print $type; ?>" class="<?php print ($counter == 1) ? "active" : ""  ?>"><?php print $type; ?></a>
+    <?php $materialCounter = 0; ?>
     <?php foreach ($subWork as $subtype => $manifestation) : ?>
-      <?php $typesarray[$type][] = array($subtype, $manifestation['translation']['subtype']); ?>
+      <?php $subMaterialCounter = count($manifestation['manifest']['manifestations']); ?>
+      <?php $typesarray[$type][] = array($subtype, $manifestation['translation']['subtype'], $subMaterialCounter); ?>
+      <?php $materialCounter += $subMaterialCounter; ?>
     <?php endforeach; ?>
+    <a href="#<?php print $type; ?>" class="<?php print ($counter == 1) ? "active" : ""  ?>"><?php print $type; ?> (<?php print $materialCounter; ?>)</a>
   <?php endforeach; ?>
 </div>
 
@@ -13,7 +16,7 @@
   <?php foreach ($typesarray as $type => $subtypes) : $counter++ ?>
     <div id="<?php print $type ?>" class="<?php print ($counter != 1) ? "visuallyhidden" : ""  ?>">
       <?php foreach ($subtypes as $subtype) : ?>
-        <a href="#<?php print $subtype[0]; ?>" class="tabs-sub-nav" ><?php print $subtype[1]; ?></a>
+        <a href="#<?php print $subtype[0]; ?>" class="tabs-sub-nav" ><?php print $subtype[1]; ?> (<?php print $subtype[2]; ?>)</a>
       <?php endforeach; ?>
     </div>
   <?php endforeach; ?>
