0827d65da0086e49f1ab6798d387331e5231e40c
Anders Tolborg <anders@tolborg.dk>
2012-11-06 14:28:43 +0100

    Fix

--- templates/views/bibdk_favourite_library.tpl.php
+++ templates/views/bibdk_favourite_library.tpl.php
@@ -5,21 +5,17 @@
  */
 //dpm($variables);
 ?>
-<div class="element-wrapper">
-  <div class="element">
-    <div class="element-section padded">
-      <div class="actions">
-        <?php print $actions; ?>
-      </div>
-      <hgroup>
-        <h3><?php print $branchName; ?></h3>
-      </hgroup>
-      <div class="toggle-next-section">
-        <a href="#">
-          <strong><?php print t('bibdk_favourite_more_info'); ?></strong>
-        </a>
-      </div>
-    </div>
-    <?php print $moreinfo; ?>
+<div class="element-section padded">
+  <div class="actions">
+    <?php print $actions; ?>
+  </div>
+  <hgroup>
+    <h3><?php print $branchName; ?></h3>
+  </hgroup>
+  <div class="toggle-next-section">
+    <a href="#">
+      <strong><?php print t('bibdk_favourite_more_info'); ?></strong>
+    </a>
   </div>
 </div>
+<?php print $moreinfo; ?>
