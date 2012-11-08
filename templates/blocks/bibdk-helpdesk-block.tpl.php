fe6fb9e3f2fa5a68f6d85fc114789d1135906bbf
Anders Tolborg <anders@tolborg.dk>
2012-11-08 22:11:32 +0100

    Fix help desk teaser block

--- templates/blocks/bibdk-helpdesk-block.tpl.php
+++ templates/blocks/bibdk-helpdesk-block.tpl.php
@@ -1,18 +1,25 @@
 <div class="span5">
- <div class="widget widget-biblioteksvagt widget-height-2">
-  <div class="inner">
-<div class="field-title">
-   <h3>
-   <?php echo $title;?>
-</h3>
-</div>
-<img src="<?php echo $image; ?>" style="width:100px"/>
-<div class="clearfix"></div>
-   <?php echo $description;?>
-   <footer>
-   <?php echo $link;?>
-   </footer>
-  </div>
- </div>
+  <article class="widget widget-biblioteksvagt widget-height-2">
+    <div class="inner">
+
+      <div class="field-title">
+        <h3><?php print $title;?></h3>
+      </div>
+
+      <div class="field-body">
+      <?php print $description;?>
+      </div>
+
+      <footer>
+        <div class="field-vagt-image">
+          <img class="field-vagt-image" src="<?php print $image; ?>" />
+        </div>
+        <div class="field-link">
+          <span class="icon icon-left icon-white-right">&nbsp;</span><?php print $link;?>
+        </div>
+      </footer>
+
+    </div>
+  </article>
 </div>
 
