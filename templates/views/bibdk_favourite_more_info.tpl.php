54e62910e0ee294139b87aee9875e0e51cae5c35
Jorgen G. Nielsen <jgn@dbc.dk>
2012-11-12 11:07:56 +0100

    refactor favorite_more_info.tpl, and update css

--- templates/views/bibdk_favourite_more_info.tpl.php
+++ templates/views/bibdk_favourite_more_info.tpl.php
@@ -9,26 +9,21 @@
 <div class="element-section visuallyhidden">
   <div class="library-details text clearfix">
     <div class="library-details-column-1">
-      <strong><?php print t('bibdk_favourite_address'); ?></strong>
-      <br/>
-      <?php print $address; ?>
-      <br/>
-      <strong><?php print t('bibdk_favourite_contact'); ?></strong>
-      <br/>
-      <?php print $contact; ?>
+      <p class="subheader"><strong><?php print t('bibdk_favourite_address'); ?></strong></p>
+      <p><?php print $address; ?></p>
+      <p class="subheader"><strong><?php print t('bibdk_favourite_contact'); ?></strong></p>
+      <p><?php print $contact; ?></p>
     </div> <!-- column-1 -->
 
     <div class="library-details-column-2">
-      <address>
-        <?php if ( $openingHours != 'ting_agency_no_opening_hours' ) { ?>
-          <p class="subheader"><strong><?php print t('bibdk_favourite_opening_hours'); ?></strong></p>
-          <p><?php print $openingHours; ?></p>
-        <?php } ?>
-        <?php if ( $tools ) { ?>
-          <p class="subheader"><strong><?php print t('bibdk_favourite_tools'); ?></strong></p>
-          <p><?php print $tools; ?></p>
-        <?php } ?>
-      </address>
+      <?php if ( $openingHours != 'ting_agency_no_opening_hours' ) { ?>
+        <p class="subheader"><strong><?php print t('bibdk_favourite_opening_hours'); ?></strong></p>
+        <p><?php print $openingHours; ?></p>
+      <?php } ?>
+      <?php if ( $tools ) { ?>
+        <p class="subheader"><strong><?php print t('bibdk_favourite_tools'); ?></strong></p>
+        <?php print $tools; ?>
+      <?php } ?>
     </div>
   </div>
 </div>
