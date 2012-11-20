013cb2d4fe46bf7a064d05d22329379ab89eeaff
Martin Møller <mmj@dbc.dk>
2012-11-20 15:09:38 +0100

    Certain links is temporarily not being rendered due to US #549

--- templates/views/ting_openformat_manifestation.tpl.php
+++ templates/views/ting_openformat_manifestation.tpl.php
@@ -1,22 +1,23 @@
 <article class="manifestation clearfix">
-  <div class="actions">
+    <div class="actions">
 
-    <div class="primary-actions">
-      <div class="btn-wrapper">
-        <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
-      </div>
-    </div>
-    <div class="secondary-actions">
-      <ul>
+        <div class="primary-actions">
+            <div class="btn-wrapper">
+              <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
+            </div>
+        </div>
+        <div class="secondary-actions">
+            <!-- <ul>
         <?php print drupal_render($fields['bibdk_linkme_permalink_default']); ?>
-      </ul>
+      </ul> -->
+        </div>
     </div>
-  </div>
-  <!-- .actions -->
-  <div class="manifestation-data text">
-    <table>
-      <tbody>
-        <?php print drupal_render($fields['ting_openformat_default_formatter']); ?>
-      </tbody></table>
-  </div>
-</article>		
+    <!-- .actions -->
+    <div class="manifestation-data text">
+        <table>
+            <tbody>
+            <?php print drupal_render($fields['ting_openformat_default_formatter']); ?>
+            </tbody>
+        </table>
+    </div>
+</article>
