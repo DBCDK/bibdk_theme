c4a6ba113926383e8c9ed38bd11b28d7e2870cbe
Søren Vibjerg <vibjerg@gmail.com>
2012-12-19 09:58:24 +0100

    Add id to toggle tabs and subtabs

--- templates/views/ting_openformat_subwork_tabs.tpl.php
+++ templates/views/ting_openformat_subwork_tabs.tpl.php
@@ -8,7 +8,7 @@
       <?php $materialCounter += $subMaterialCounter; ?>
     <?php endforeach; ?>
     <?php if ($type) : //if statement should be removed when openFormat is in production. If-statement temporarily included to avoid printing "polluted" data from OpenFormat?> 
-      <a href="#<?php print $type; ?>" class="<?php print ($counter == 1) ? "active" : ""  ?>"><?php print $type; ?> (<?php print $materialCounter; ?>)</a>
+      <a href="#<?php print $type; ?><?php print $ding_id; ?>" class="<?php print ($counter == 1) ? "active" : ""  ?>"><?php print $type; ?> (<?php print $materialCounter; ?>)</a>
     <?php endif; ?>
   <?php endforeach; ?>
 </div>
@@ -17,9 +17,9 @@
   <?php $outercounter = 0; ?>
   <?php foreach ($typesarray as $type => $subtypes) : $outercounter++ ?>
     <?php $innercounter = 0; ?>
-    <div id="<?php print $type ?>" class="<?php print ($outercounter != 1) ? "visuallyhidden" : ""  ?>">
+    <div id="<?php print $type ?><?php print $ding_id; ?>" class="<?php print ($outercounter != 1) ? "visuallyhidden" : ""  ?>">
       <?php foreach ($subtypes as $subtype) : $innercounter++ ?>
-        <a href="#<?php print $subtype[0]; ?>" class="<?php print ($innercounter == 1) ? "active" : ""  ?>"><?php print $subtype[1]; ?> (<?php print $subtype[2]; ?>)</a>
+        <a href="#<?php print $subtype[0]; ?><?php print $ding_id; ?>" class="<?php print ($innercounter == 1) ? "active" : ""  ?>"><?php print $subtype[1]; ?> (<?php print $subtype[2]; ?>)</a>
       <?php endforeach; ?>
     </div>
   <?php endforeach; ?>
