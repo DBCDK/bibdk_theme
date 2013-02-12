9e11600e08b03f04830ed3b73cb6085aaf02cd14
Martin Møller <mmj@dbc.dk>
2013-02-12 11:01:22 +0100

    bibdk_cart btw styling

--- templates/fields/bibdk-cart-button.tpl.php
+++ templates/fields/bibdk-cart-button.tpl.php
@@ -1,6 +1,4 @@
-<li>
-    <a href="<?php print $add_to_cart['#href']; ?>"
-       class="linkme-button text-small text-darkgrey">
-        <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><?php print $add_to_cart['#title']; ?>
-    </a>
-</li>
+<a href="<?php print $add_to_cart['#href']; ?>"
+   class="linkme-button text-small text-darkgrey">
+    <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><?php print $add_to_cart['#title']; ?>
+</a>
