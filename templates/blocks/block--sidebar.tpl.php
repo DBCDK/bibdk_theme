73588392cdf09a02a2a576ca2f60861c618ffafd
JrgenGNielsen <jgn@dbc.dk>
2013-02-14 15:31:58 +0100

    theming for seceondary tabs in user pages, with bibdk_mypage

--- templates/blocks/block--sidebar.tpl.php
+++ templates/blocks/block--sidebar.tpl.php
@@ -1,8 +1,10 @@
 <?php if (isset($variables['elements']['#secondary'])) : ?>
   <div id="sidebar">
-    <div class="sidebar-header">
-      <?php print ( isset($variables['elements']['#bibdk_mypage_tab']) ) ? drupal_render($variables['elements']['#bibdk_mypage_tab']) : t('My page'); ?>
-    </div>
+    <nav class="sidebar-header">
+      <ul>
+        <?php print ( isset($variables['elements']['#bibdk_mypage_tab']) ) ? drupal_render($variables['elements']['#bibdk_mypage_tab']) : '<li>' . t('My page') . '</li>'; ?>
+      </ul>
+    </nav>
     <nav class="sidebar-nav">
       <ul>
         <?php print drupal_render($variables['elements']['#primary']); ?>
