2027c3cb6134b2bcdc6bb12793192ecff22971c2
Martin Møller <mmj@dbc.dk>
2013-02-20 15:22:11 +0100

    Code formatting

--- templates/views/bibdk-cart-manifestation.tpl.php
+++ templates/views/bibdk-cart-manifestation.tpl.php
@@ -1,24 +1,13 @@
-
 <article class="clearfix cart-item-id-<?php print $pid; ?>" xmlns="http://www.w3.org/1999/html">
-    <!--
-    <div class="actions">
-
-        <div class="primary-actions">
-            <div class="btn-wrapper">
-              <?php print drupal_render($fields['bibdk_mani_reservation_button']); ?>
-            </div>
-        </div>
-    </div>
-    -->
-    <!-- .actions -->
     <div class="manifestation-data text">
       <?php if (!empty($title)) : ?>
-        <h6><?php print $title; ?><span class="italic normal"> - <?php print $type; ?> <?php print $several_editions; ?></span></h6>
+        <h6><?php print $title; ?>
+            <span class="italic normal"> - <?php print $type; ?> <?php print $several_editions; ?></span>
+        </h6>
       <?php endif; ?>
 
       <?php if (!empty($author)) : ?>
         <p><?php print $author; ?></p>
       <?php endif; ?>
-
     </div>
 </article>
