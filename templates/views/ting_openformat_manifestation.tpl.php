e94e1e1add6c6881332aec6a326470d0e0b9f202
Martin Møller <mmj@dbc.dk>
2013-03-19 11:34:44 +0100

    Stylebug fix

--- templates/views/ting_openformat_manifestation.tpl.php
+++ templates/views/ting_openformat_manifestation.tpl.php
@@ -1,25 +1,30 @@
 <article class="manifestation clearfix">
-    <div class="actions">
-
-        <div class="primary-actions">
-            <div class="btn-wrapper">
-              <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
-            </div>
-          <?php print drupal_render($fields['bibdk_cart_link_default']); ?>
-        </div>
-        <div class="secondary-actions">
-            <ul>
-              <?php print drupal_render($fields['bibdk_linkme_permalink_default']); ?>
-              <?php print drupal_render($fields['bibdk_holdingstatus_favourite_default']); ?>
-            </ul>
-        </div>
+  <div class="actions">
+    <?php dpm($fields); ?>
+    <div class="primary-actions">
+      <div class="btn-wrapper">
+        <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
+      </div>
+      <?php print drupal_render($fields['bibdk_cart_link_default']); ?>
     </div>
-    <!-- .actions -->
-    <div class="manifestation-data text">
-        <table>
-            <tbody>
-            <?php print drupal_render($fields['ting_openformat_default_formatter']); ?>
-            </tbody>
-        </table>
+    <div class="secondary-actions">
+      <ul>
+        <?php print drupal_render($fields['bibdk_linkme_permalink_default']); ?>
+        <?php print drupal_render($fields['bibdk_holdingstatus_favourite_default']); ?>
+      </ul>
     </div>
+  </div>
+  <!-- .actions -->
+  <div class="manifestation-data text">
+    <table>
+      <tbody>
+      <?php foreach ($fields['ting_openformat_default_formatter'] as $field) : ?>
+        <tr>
+          <th><?php print $field['#title']; ?></th>
+          <td><?php print $field[0]['#markup']; ?></td>
+        </tr>
+      <? endforeach; ?>
+      </tbody>
+    </table>
+  </div>
 </article>
