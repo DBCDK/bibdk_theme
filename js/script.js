51091646128fe4d237343637bccae41d8ca2fe31
JrgenGNielsen <jgn@dbc.dk>
2013-05-06 13:49:40 +0200

    some javascript stuff

--- js/script.js
+++ js/script.js
@@ -100,7 +100,9 @@
             });
 
             // Select side of work cover
-            $('.work-cover-selector a').once().click(function(e) {
+            // For now, we open the back cover pdf in a popup on click on .work-cover-back bibdk-popup-link jquery-once-1-processed
+            $('.work-cover-selector a.work-cover-front').once().click(function(e) {
+            // $('.work-cover-selector a').once().click(function(e) {
                 e.preventDefault();
 
                 var index = $(this).index() + 1;
@@ -320,6 +322,18 @@
                     resizable:1,
                     left:35,
                     top:35
+                }
+                ,
+                backcoverpdf: {
+                    height:900,
+                    width:650,
+                    center:0,
+                    createnew:0,
+                    scrollbars:1,
+                    status:0,
+                    resizable:1,
+                    left:40,
+                    top:40
                 }
             };
 
