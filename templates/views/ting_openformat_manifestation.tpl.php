5aca8f951deeee7c52547f0bbc0a21dac7ae9003
Anders-Andersen <ana@dringo.dbc.dk>
2013-05-15 13:26:39 +0200

    879_link_favourite_library

--- templates/views/ting_openformat_manifestation.tpl.php
+++ templates/views/ting_openformat_manifestation.tpl.php
@@ -4,12 +4,12 @@
       <div class="btn-wrapper">
         <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
       </div>
+      <?php print drupal_render($fields['bibdk_holdingstatus_favourite_default']); ?>
       <?php print drupal_render($fields['bibdk_cart_link_default']); ?>
     </div>
     <div class="secondary-actions">
       <ul>
         <?php print drupal_render($fields['bibdk_linkme_permalink_default']); ?>
-        <?php print drupal_render($fields['bibdk_holdingstatus_favourite_default']); ?>
       </ul>
     </div>
   </div>
