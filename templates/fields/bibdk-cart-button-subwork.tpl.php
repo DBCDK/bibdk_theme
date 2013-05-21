52dfae4f152b908e6eecd49417b91f88ddea3331
pjohans <pjohans@gmail.com>
2013-05-21 13:01:27 +0200

    851 move add-to-cart-link

--- templates/fields/bibdk-cart-button-subwork.tpl.php
+++ templates/fields/bibdk-cart-button-subwork.tpl.php
@@ -1,7 +1,7 @@
-<a href="<?php print $add_to_cart['#href']; ?>"
+<a href="<?php print $add_to_cart['#href'];?>"
    class="text-small">
-    <span class="icon icon-left icon-blue-addbasket">&nbsp;</span><span
-        class="link-add-basket cart-title"
-        data-pid="<?php print $add_to_cart['#data-pid']; ?>"><?php print $add_to_cart['#title']; ?>
+    <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><span
+        class="link-add-basket cart-title text-lightgrey"
+        data-pid="<?php print $add_to_cart['#data-pid']; ?>"><?php print $add_to_cart['#title'];?>
     </span>
 </a>
