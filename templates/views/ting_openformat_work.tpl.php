a230cb377ff606b39ff4509ccd636ab5b04a282f
pjohans <pjohans@gmail.com>
2013-05-21 13:02:31 +0200

    851 delete unused code

--- templates/views/ting_openformat_work.tpl.php
+++ templates/views/ting_openformat_work.tpl.php
@@ -8,6 +8,7 @@
   <div class="wrapper">
     <?php print drupal_render($fields); ?>
     <?php print $work_tabs ?>
+    
     <!-- tabs -->
   </div>
   <!-- wrapper -->
@@ -17,6 +18,7 @@
 <!-- work-share-data -->
 <div class="bibdk-tabs bibdk-tabs-heavy">
   <?php print $tabs; ?>
+  
   <!-- tabs-nav -->
   <div class="tabs-sections">
     <?php $count = 0; ?>
@@ -25,13 +27,6 @@
         <div id="<?php print $subtype; ?><?php print $ding_id; ?>"
              class="subwork tabs-section <?php print ($count != 1) ? "visuallyhidden" : "" ?>">
           <div class="padded text clearfix">
-            <div class="actions">
-              <div class="primary-actions">
-                <div class="btn-wrapper">
-                </div>
-                <?php print drupal_render($manifest['manifest']['subtype_actions']['cart']); ?>
-              </div>
-            </div>
             <?php echo drupal_render($manifest['manifest']['subtype_actions']); ?>
           </div>
           <!-- tabs-content -->
