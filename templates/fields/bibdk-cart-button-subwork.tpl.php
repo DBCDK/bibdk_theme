e8b51110a8b9c6ede44f21f0d778b2389422f6aa
Søren Vibjerg <vibjerg@gmail.com>
2013-06-07 16:09:01 +0200

    remove unused tpl

--- templates/fields/bibdk-cart-button-subwork.tpl.php
+++
@@ -1,7 +0,0 @@
-<a href="<?php print $add_to_cart['#href'];?>"
-   class="text-small">
-    <span class="icon icon-left icon-darkgrey-addbasket">&nbsp;</span><span
-        class="link-add-basket cart-title text-lightgrey"
-        data-pid="<?php print $add_to_cart['#data-pid']; ?>"><?php print $add_to_cart['#title'];?>
-    </span>
-</a>
