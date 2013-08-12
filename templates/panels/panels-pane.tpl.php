774d12978a63e9c5944dda2ccbce384562bd2bf8
JrgenGNielsen <jgn@dbc.dk>
2013-08-12 11:25:39 +0200

    fix bug in panels-pane.tpl.php

--- templates/panels/panels-pane.tpl.php
+++ templates/panels/panels-pane.tpl.php
@@ -1,37 +1,40 @@
 <?php
 /**
-* @file panels-pane.tpl.php
-* Main panel pane template
-*
-* Variables available:
-* - $pane->type: the content type inside this pane
-* - $pane->subtype: The subtype, if applicable. If a view it will be the
-*   view name; if a node it will be the nid, etc.
-* - $title: The title of the content
-* - $content: The actual content
-* - $links: Any links associated with the content
-* - $more: An optional 'more' link (destination only)
-* - $admin_links: Administrative links associated with the content
-* - $feeds: Any feed icons or associated with the content
-* - $display: The complete panels display object containing all kinds of
-*   data including the contexts and all of the other panes being displayed.
-*/
+ * @file panels-pane.tpl.php
+ * Main panel pane template
+ *
+ * Variables available:
+ * - $pane->type: the content type inside this pane
+ * - $pane->subtype: The subtype, if applicable. If a view it will be the
+ *   view name; if a node it will be the nid, etc.
+ * - $title: The title of the content
+ * - $content: The actual content
+ * - $links: Any links associated with the content
+ * - $more: An optional 'more' link (destination only)
+ * - $admin_links: Administrative links associated with the content
+ * - $feeds: Any feed icons or associated with the content
+ * - $display: The complete panels display object containing all kinds of
+ *   data including the contexts and all of the other panes being displayed.
+ */
 
 if ( $title && $pane->configuration['override_title'] && $pane->configuration['override_title_text']) {
   $title = t($title, array(), array('context' => 'panels_pane_title'));
 }
-?>
 
+?>
+<?php if ($pane_prefix): ?>
+  <?php print $pane_prefix; ?>
+<?php endif; ?>
 <div class="<?php print $classes; ?>" <?php print $id; ?>>
   <?php if ($admin_links): ?>
-    <div class="admin-links panel-hide">
-      <?php print $admin_links; ?>
-    </div>
+    <?php print $admin_links; ?>
   <?php endif; ?>
 
+  <?php print render($title_prefix); ?>
   <?php if ($title): ?>
-    <h2 class="pane-title"><?php print $title; ?></h2>
+    <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
   <?php endif; ?>
+  <?php print render($title_suffix); ?>
 
   <?php if ($feeds): ?>
     <div class="feed">
@@ -40,7 +43,7 @@ if ( $title && $pane->configuration['override_title'] && $pane->configuration['o
   <?php endif; ?>
 
   <div class="pane-content">
-    <?php print $content; ?>
+    <?php print render($content); ?>
   </div>
 
   <?php if ($links): ?>
@@ -55,3 +58,6 @@ if ( $title && $pane->configuration['override_title'] && $pane->configuration['o
     </div>
   <?php endif; ?>
 </div>
+<?php if ($pane_suffix): ?>
+  <?php print $pane_suffix; ?>
+<?php endif; ?>
