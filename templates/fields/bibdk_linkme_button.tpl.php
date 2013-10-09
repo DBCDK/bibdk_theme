efa57c5351d6af425f96344c2bdd983c24dc6982
Martin Møller <mmj@dbc.dk>
2013-10-09 14:27:28 +0200

    Codeformatting

--- templates/fields/bibdk_linkme_button.tpl.php
+++ templates/fields/bibdk_linkme_button.tpl.php
@@ -1,11 +1,11 @@
 <div>
-    <a href="#<?php print $path; ?>"
-       class="popover-button text-small text-lightgrey"><span
-            class="icon icon-left icon-lightgrey-link">&nbsp;</span><?php print $link_text; ?>
-    </a>
+  <a href="#<?php print $path; ?>"
+     class="popover-button text-small text-lightgrey"><span
+      class="icon icon-left icon-lightgrey-link">&nbsp;</span><?php print $link_text; ?>
+  </a>
 
-    <div class='popover element-wrapper visuallyhidden linkme-wrapper'>
-        <div class=''><?php print drupal_render_children($link_form) ?><span
-                class="close icon icon-left icon-red-x">&nbsp;</span></div>
-    </div>
+  <div class='popover element-wrapper visuallyhidden linkme-wrapper'>
+    <div class=''><?php print drupal_render_children($link_form) ?><span
+        class="close icon icon-left icon-red-x">&nbsp;</span></div>
+  </div>
 </div>
