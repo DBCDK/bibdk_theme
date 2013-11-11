2afeb292930a02818e36a242b1f6cf0363a6d03f
Anders-Andersen <ana@dringo.dbc.dk>
2013-11-11 11:45:55 +0100

    Bug 16558 - titlespecific

--- templates/views/bibdk-cart-manifestation.tpl.php
+++ templates/views/bibdk-cart-manifestation.tpl.php
@@ -7,8 +7,8 @@
         <span class="italic normal"> - <?php print $type_translated['type']; ?> <?php print $several_editions; ?></span>
       </h6>
 
-      <?php hide($fields['bibdk_manbibdk_mani_title_specifici_title']); ?>
       <?php hide($fields['bibdk_mani_title_specific']); ?>
+      <?php hide($fields['bibdk_mani_type']); ?>
     <?php endif; ?>
     <?php print drupal_render($fields); ?>
   </div>
