6c013d4c8cd78059ee68cfaed51be1fb7f010c1f
Anders-Andersen <ana@dringo.dbc.dk>
2013-11-11 11:45:18 +0100

    Bug 16558 - titlespecific

--- templates/views/bibdk-cart-manifestation.tpl.php
+++ templates/views/bibdk-cart-manifestation.tpl.php
@@ -1,14 +1,14 @@
 <article class="clearfix cart-item-id-<?php print $pid; ?>">
   <div class="manifestation-data text">
 
-    <?php if (!empty($fields['bibdk_mani_title'])) : ?>
+    <?php if (!empty($fields['bibdk_mani_title_specific'])) : ?>
       <h6>
-        <?php print $fields['bibdk_mani_title'][0]['#markup']; ?>
+        <?php print $fields['bibdk_mani_title_specific'][0]['#markup']; ?>
         <span class="italic normal"> - <?php print $type_translated['type']; ?> <?php print $several_editions; ?></span>
       </h6>
 
-      <?php hide($fields['bibdk_mani_title']); ?>
-      <?php hide($fields['bibdk_mani_type']); ?>
+      <?php hide($fields['bibdk_manbibdk_mani_title_specifici_title']); ?>
+      <?php hide($fields['bibdk_mani_title_specific']); ?>
     <?php endif; ?>
     <?php print drupal_render($fields); ?>
   </div>
