7614d7251340078957499924a2d322867461ba46
JrgenGNielsen <jgn@dbc.dk>
2013-11-26 18:08:57 +0100

    fixed "close" icon

--- templates/blocks/user-alert.tpl.php
+++ templates/blocks/user-alert.tpl.php
@@ -16,7 +16,7 @@ if ( empty($body) ) {
 
 <div id="user-alert-<?php print $nid; ?>" class="user-alert">
 <?php if ($is_closeable) : ?>
-  <div class="user-alert-close"><a href="javascript:;" rel="<?php print $nid; ?>">x</a>
+  <div class="user-alert-close"><a href="javascript:;" rel="<?php print $nid; ?>"><span class="user-alert-close-image"></span></a>
 </div><?php endif; ?>
   <div class="user-alert-message">
     <?php if ($alert_label) : ?><span class="user-label"><?php print $alert_label; ?>:</span><?php endif; ?>
