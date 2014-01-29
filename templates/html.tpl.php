e423015d94dfa72e411ecc177690a6367bbf50ca
Anders Tolborg <anders@tolborg.dk>
2014-01-29 10:36:30 +0100

    Remove ”Dosis” font. It is not used anywhere

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,19 +1,10 @@
 <!DOCTYPE html>
 <html xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-
 <head>
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <style type="text/css">
-    @font-face {
-        font-family: "Dosis";
-        font-style: normal;
-        font-weight: 400;
-        src: local("Dosis Regular"), local("Dosis-Regular"), url("<?php echo file_create_url(drupal_get_path('theme', 'bibdk_theme')); ?>/dosis.woff") format("woff");
-    }
-  </style>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page_top; ?>
