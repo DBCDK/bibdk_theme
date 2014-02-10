e6ade0a1e3f8ad8cb1b3871f863c081ca5db606e
Martin Møller <mmj@dbc.dk>
2014-02-10 11:32:18 +0100

    Accessibility features added

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
-<head>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-</head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-</body>
+  <head>
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+  </head>
+  <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+    <div id="mainwrapper" aria-hidden="false">
+      <?php print $page_top; ?>
+      <?php print $page; ?>
+      <?php print $page_bottom; ?>
+    </div>
+  </body>
 </html>
