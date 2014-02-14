9b54a8ba03cf3f8461604cd5b239f35ade984433
Michael Carlsson <mca@dbc.dk>
2014-02-14 13:20:28 +0100

    Focus is placed when page is loading.

--- js/script.js
+++ js/script.js
@@ -447,20 +447,6 @@
         });
       }
 
-      // ****************************  CURSOR POSITIONS **************************** //
-      // Default in search block form - unless it's a search result.
-      $('form#search-block-form input[name="search_block_form"]').not('.page-search form#search-block-form input[name="search_block_form"], .page-vejviser form#search-block-form input[name="search_block_form"]').focus();
-      // Helpdesk popup
-      $('.page-overlay-helpdesk').find('input[type=text], textarea').filter(':visible:first').focus();
-      // User login form
-      $('form#user-login').find('input[type=text], textarea').filter(':visible:first').focus();
-      // Create new account
-      $('form#user-register-form').find('input[type=text], textarea').filter(':visible:first').focus();
-      // Request new password
-      $('form#user-pass').find('input[type=text], textarea').filter(':visible:first').focus();
-      // User help popup
-      $('#bibdk-help-search-form #edit-search-help').filter(':visible').focus();
-
       // ****************************  popovers **************************** //
       // SEARCH SORT DROPDOWN
       $('.bibdk-search-controls-form .dropdown-menu li a').click(function() {
