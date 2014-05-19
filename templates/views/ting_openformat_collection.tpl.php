ee3ab3af9747e15574d19fd06e5a91a8fe936cc7
JrgenGNielsen <jgn@dbc.dk>
2014-05-19 15:13:09 +0200

    alter work header element sequence to suit screenreaders

--- templates/views/ting_openformat_collection.tpl.php
+++ templates/views/ting_openformat_collection.tpl.php
@@ -1,32 +1,46 @@
 <div class="work element-wrapper <?php print $work_type; ?>">
+
   <div class="element">
+
     <div class="work-header element-section">
-      <?php print drupal_render($actions); ?>
-      <!-- actions -->
-      <?php print $types; ?>
 
       <div class="element-title">
         <div>
           <h2><?php print $title; ?></h2>
-
           <h3><?php print $author; ?></h3>
           <?php if (isset($partOf)) : ?>
             <span>I: <?php print $partOf; ?></span>
           <?php endif; ?>
         </div>
       </div>
-      <div class="msg-<?php print $uid; ?> collection-msg"></div>
-    <?php print $togglework; ?>
+
+      <div class="element-actions">
+        <?php print drupal_render($actions); ?>
+      </div>
+
+      <div class="element-types">
+        <?php print $types; ?>
+      </div>
+
+      <div class="msg-<?php print $uid; ?> collection-msg clearfix"></div>
+
+      <?php print $togglework; ?>
+
     </div>
+
     <!-- element-section (work-header) -->
-    <?php if(isset($work_one)): ?>
+    <?php if ( isset($work_one) ): ?>
+
       <div class="work-body work-body-has-cover element-section">
         <div><?php print $work_one; ?></div>
       </div>
+
     <?php else: ?>
+
       <div class="work-body work-body-has-cover element-section visuallyhidden">
         <div id="ajax_placeholder_<?php print $uid; ?>"></div>
       </div>
+
     <?php endif; ?>
     <!-- element-section -->
   </div>
