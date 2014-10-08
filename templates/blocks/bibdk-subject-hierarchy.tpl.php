4fcd0f288017839d373237ad5f9948f2980f7162
Anders-Andersen <ana@dringo.dbc.dk>
2014-10-08 12:33:20 +0200

    feature/1429_help-icon

--- templates/blocks/bibdk-subject-hierarchy.tpl.php
+++ templates/blocks/bibdk-subject-hierarchy.tpl.php
@@ -14,9 +14,7 @@
       </div>
       <div class="bibdk-subject-hierarchy-helptext">
         <p class="helptext popover-button" title="<?php print $helptext; ?>">
-          <a href="#" tabindex="0" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>" 
-             role="button" class="close icon icon-left icon-red-x "> 
-          </a>
+          <a href="#" tabindex="0" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
         </p>
         <div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
           <p class="user-msg ">
