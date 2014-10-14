044ce0b5baa26dcc8c84f6f62dd02e6a96622607
Martin Møller <mmj@dbc.dk>
2014-10-14 15:45:05 +0200

    frontpage styling

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
   <head>
+    <meta name="viewport" content="initial-scale=1">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
     <?php print $styles; ?>
