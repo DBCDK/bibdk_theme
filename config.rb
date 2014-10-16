53fd3dab91d2347ec1252e4878b73d93b0c19827
Martin Møller <mmj@dbc.dk>
2014-10-16 15:30:03 +0200

    added production options in config file

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
@@ -15,7 +15,7 @@ relative_assets = true
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-sourcemap = true
+sourcemap = (environment == :production) ? false : true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
