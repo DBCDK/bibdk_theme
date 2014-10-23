658bd4fd1cfab92026850f20669b4a4d96ff4819
Martin Møller <mmj@dbc.dk>
2014-10-23 13:22:39 +0200

    Foundation added

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+# Require any additional compass plugins here.
+add_import_path "foundation/scss"
+
 # Set this to the root of your project when deployed:
 # http_path = "/"
 css_dir = "css"
@@ -13,7 +16,7 @@ output_style = (environment == :production) ? :compressed : :expanded
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = (environment == :production) ? false : true
 
 sourcemap = (environment == :production) ? false : true
 
@@ -23,3 +26,8 @@ sourcemap = (environment == :production) ? false : true
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+#
+if Gem.loaded_specs["sass"].version >= Gem::Version.create('3.4')
+  warn "You're using Sass 3.4 or higher to compile Foundation. This version causes CSS classes to output incorrectly, so we recommend using Sass 3.3 or 3.2."
+  warn "To use the right version of Sass on this project, run \"bundle\" and then use \"bundle exec compass watch\" to compile Foundation. Requires bundler to be install (gem install bundler)"
+end
