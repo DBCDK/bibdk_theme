145489896e3d9ae097a3609f9c07f1363e7c78e7
Martin Møller <mmj@dbc.dk>
2014-11-04 15:01:29 +0100

    codeformatting

--- js/lib/jquery.popupwindow.js
+++ js/lib/jquery.popupwindow.js
@@ -1,83 +1,76 @@
 var myWindow = myWindow ? myWindow : false;
 
-jQuery.fn.popupwindow = function(p)
-{
+jQuery.fn.popupwindow = function(p) {
 
   var profiles = p || {};
 
-  return this.each(function(index){
+  return this.each(function(index) {
     var settings, parameters, mysettings, b, a;
 
     // for overrideing the default settings
-    mysettings       = (jQuery(this).attr("rel") || "").split(",");
-    mysettings_html5 = (jQuery(this).attr("data-rel") || "").split(",");
+    mysettings = (jQuery(this).attr("rel") || "").split(",");
+    var mysettings_html5 = (jQuery(this).attr("data-rel") || "").split(",");
     mysettings = ( mysettings_html5.length > 0 ) ? mysettings_html5 : mysettings;
 
     settings = {
-      height:600, // sets the height in pixels of the window.
-      width:600, // sets the width in pixels of the window.
-      toolbar:0, // determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}.
-      scrollbars:0, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
-      status:0, // whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}.
-      resizable:1, // whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.
-      left:0, // left position when the window appears.
-      top:0, // top position when the window appears.
-      center:0, // should we center the window? {1 (YES) or 0 (NO)}. overrides top and left
-      createnew:1, // should we create a new window for each occurance {1 (YES) or 0 (NO)}.
-      location:0, // determines whether the address bar is displayed {1 (YES) or 0 (NO)}.
-      menubar:0 // determines whether the menu bar is displayed {1 (YES) or 0 (NO)}.
+      height: 600, // sets the height in pixels of the window.
+      width: 600, // sets the width in pixels of the window.
+      toolbar: 0, // determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}.
+      scrollbars: 0, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
+      status: 0, // whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}.
+      resizable: 1, // whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.
+      left: 0, // left position when the window appears.
+      top: 0, // top position when the window appears.
+      center: 0, // should we center the window? {1 (YES) or 0 (NO)}. overrides top and left
+      createnew: 1, // should we create a new window for each occurance {1 (YES) or 0 (NO)}.
+      location: 0, // determines whether the address bar is displayed {1 (YES) or 0 (NO)}.
+      menubar: 0 // determines whether the menu bar is displayed {1 (YES) or 0 (NO)}.
     };
 
     // if mysettings length is 1 and not a value pair then assume it is a profile declaration
     // and see if the profile settings exists
 
-    if(mysettings.length == 1 && mysettings[0].split(":").length == 1)
-    {
+    if(mysettings.length == 1 && mysettings[0].split(":").length == 1) {
       a = mysettings[0];
       // see if a profile has been defined
-      if(typeof profiles[a] != "undefined")
-      {
+      if(typeof profiles[a] != "undefined") {
         settings = jQuery.extend(settings, profiles[a]);
       }
     }
-    else
-    {
+    else {
       // overrides the settings with parameter passed in using the rel tag.
-      for(var i=0; i < mysettings.length; i++)
-      {
+      for(var i = 0; i < mysettings.length; i++) {
         b = mysettings[i].split(":");
-        if(typeof settings[b[0]] != "undefined" && b.length == 2)
-        {
+        if(typeof settings[b[0]] != "undefined" && b.length == 2) {
           settings[b[0]] = b[1];
         }
       }
     }
 
     // center the window
-    if (settings.center == 1)
-    {
-      settings.top = (screen.height-(settings.height + 110))/2;
-      settings.left = (screen.width-settings.width)/2;
+    if(settings.center == 1) {
+      settings.top = (screen.height - (settings.height + 110)) / 2;
+      settings.left = (screen.width - settings.width) / 2;
     }
 
-    parameters = "location=" + settings.location + ",menubar=" + settings.menubar + ",height=" + settings.height + ",width=" + settings.width + ",toolbar=" + settings.toolbar + ",scrollbars=" + settings.scrollbars  + ",status=" + settings.status + ",resizable=" + settings.resizable + ",left=" + settings.left  + ",screenX=" + settings.left + ",top=" + settings.top  + ",screenY=" + settings.top;
+    parameters = "location=" + settings.location + ",menubar=" + settings.menubar + ",height=" + settings.height + ",width=" + settings.width + ",toolbar=" + settings.toolbar + ",scrollbars=" + settings.scrollbars + ",status=" + settings.status + ",resizable=" + settings.resizable + ",left=" + settings.left + ",screenX=" + settings.left + ",top=" + settings.top + ",screenY=" + settings.top;
 
     orderedonce = jQuery(this).hasClass('orderedOnceWork');
 
-    jQuery(this).bind("click.orderPopup", function(event){
-      if ( orderedonce ) {
+    jQuery(this).bind("click.orderPopup", function(event) {
+      if(orderedonce) {
         var test = confirm(Drupal.t("You have already ordered this item once. Continue?"));
-        if(test == false){
+        if(test == false) {
           $(this).unbind(event);
           return false;
         }
       }
       var name = settings.createnew ? "PopUpWindow" + index : "PopUpWindow";
-      if ( myWindow && settings.createnew ) { // close other popups, if it opens in a new window
+      if(myWindow && settings.createnew) { // close other popups, if it opens in a new window
         myWindow.close();
       }
       myWindow = window.open(this.href, name, parameters);
-      if ( myWindow == null || typeof(myWindow)=='undefined' ) {
+      if(myWindow == null || typeof(myWindow) == 'undefined') {
         alert(Drupal.t("It looks like you have a pop-up blocker. Please push the button again."));
       }
       myWindow.focus();
