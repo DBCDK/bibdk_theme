afc8d455002970ab342fcfd388240962b42d556f
Martin Møller <mmj@dbc.dk>
2014-11-11 15:45:27 +0100

    added optional label in offcanvas menu

--- templates/topbar/bibdk-links-list.tpl.php
+++ templates/topbar/bibdk-links-list.tpl.php
@@ -1,14 +1,24 @@
 <?php
 /**
  * @file
- * HEST!
- * - $attributes:
- * - $items:
- *  - $item:
+ * Renders a unordered list of links. Otherwise Drupal's standard list it is
+ * possible to set attributes on all elements in the list.
+ *
+ * @parameters:
+ * - $attributes: array with attributes that should be attached the outer list element (<ul>).
+ * - $items: array with items. Each item will represent a link i the list.
+ *  - $item: a link that will be wrapped in a <li>.
+ * $label: if a label is provided it will be printed wrapped in a <li> at top of the list.
+ *
+ * @see _bibdk_theme_get_offcanvas_menu_list() in template.php for example of usage.
  */
 ?>
 
 <ul<?php print drupal_attributes($attributes);?>>
+  <?php if($label): ?>
+    <li><label><?php print $label; ?></label></li>
+  <?php endif; ?>
+
   <?php foreach($items as $item): ?>
     <li<?php print drupal_attributes($item['li_attributes']); ?>><?php print $item['link']; ?></li>
   <?php endforeach; ?>
