b686e0a4d4cb6685527911db8e49c6477cc32083
Martin Møller <mmj@dbc.dk>
2014-12-04 15:05:23 +0100

    attributes are rendered within the template

--- templates/global/link-with-svg.tpl.php
+++ templates/global/link-with-svg.tpl.php
@@ -7,15 +7,17 @@
  * Available variables:
  *   - $title: Title of the link
  *   - $href: The path the link should point to
- *   - $attributes: Attributes that should be provided with the link
+ *   - $attributes: Array of attributes
  *   - $svg: id of the svg-icon that should be displayed. This value will also
  *           be used as the class for the svg element so one should enusre that
  *           class is matched in the CSS.
+ *
+ * @see drupal_attributes()
  */
 ?>
 
 
-<a href="<?php print $href; ?>" <?php print $attributes; ?> >
+<a href="<?php print $href; ?>" <?php print drupal_attributes($attributes); ?> >
   <svg class="icon <?php print $svg; ?>">
     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#<?php print $svg; ?>"></use>
   </svg>
