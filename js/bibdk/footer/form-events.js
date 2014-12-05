e0c9ca22b7b4a45dc492aeeec0bb07a796d08ca5
Martin Møller <mmj@dbc.dk>
2014-12-05 13:55:19 +0100

    setting focus in input field when the containing div is clicked

--- js/bibdk/footer/form-events.js
+++ js/bibdk/footer/form-events.js
@@ -7,7 +7,11 @@
 (function($) {
   Drupal.behaviors.formEvents = {
     attach: function(context, settings) {
-      var $formItems = $('#bibdk-modal ');
+      $('#bibdk-modal input[type="text"], input[type="password"]').each(function(key, item){
+        $(this).parent().on('click', function(e){
+          $('input', this).focus();
+        })
+      });
     }
   };
 })(jQuery);
