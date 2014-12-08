76c33b010f7b137356053d54110e45447d1af7bf
Martin Møller <mmj@dbc.dk>
2014-12-08 15:55:36 +0100

    all input fields gets an eventlistener

--- js/bibdk/footer/form-events.js
+++ js/bibdk/footer/form-events.js
@@ -7,7 +7,7 @@
 (function($) {
   Drupal.behaviors.formEvents = {
     attach: function(context, settings) {
-      $('#bibdk-modal input[type="text"], input[type="password"]').each(function(key, item){
+      $('input[type="text"], input[type="password"]').each(function(key, item){
         $(this).parent().on('click', function(e){
           $('input', this).focus();
         })
