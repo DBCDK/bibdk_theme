2ca08f084787a03f1e93b1ca267e5e804c391294
JrgenGNielsen <jgn@dbc.dk>
2014-12-10 16:43:41 +0100

    restyle "order any" buttons and add svg icons

--- templates/global/span-with-svg.tpl.php
+++ templates/global/span-with-svg.tpl.php
@@ -17,8 +17,10 @@
 
 
 <span <?php print drupal_attributes($attributes); ?> >
+<?php if ($svg): ?>
   <svg class="icon <?php print $svg; ?>">
     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#<?php print $svg; ?>"></use>
   </svg>
+<?php endif; ?>
   <?php print drupal_render($content); ?>
 </a>
