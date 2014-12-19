b549ce8387abc6459801a1c877efe5e6e4aba403
Martin Møller <mmj@dbc.dk>
2014-12-19 13:12:05 +0100

    refactored content of eventhandlers into several methods

--- js/bibdk/footer/modal-events.js
+++ js/bibdk/footer/modal-events.js
@@ -9,41 +9,63 @@
   $(document).on('open.fndtn.reveal', '[data-reveal]', function() {
     $('.exit-off-canvas').trigger('click');
 
-    var $mainwrapper = $("#mainwrapper");
     if(!Modernizr.touch) { // blur the background but only if we're aren't on a touch device as the blur effect is performance heavy
-      $mainwrapper.css("filter", "blur(20px)");
-      $mainwrapper.css("-webkit-filter", "blur(20px)");
-      $mainwrapper.css("-moz-filter", "blur(20px)");
-      $mainwrapper.css("-o-filter", "blur(20px)");
-      $mainwrapper.css("-ms-filter", "blur(20px)");
+      addFilterOnMainwrapper();
     }
   });
 
 // attaching behaviors when the modal have finished opening
   $(document).on('opened.fndtn.reveal', '[data-reveal]', function() {
-    var modal = document.getElementById('bibdk-modal');
-    Drupal.attachBehaviors(modal, null);
+    toggleBehaviorsOnModal(true);
+
     Drupal.bibdkModal.addAccessibilityInfo(window.document);
     onLoad.setFocus();
 
-    var $mainwrapper = $("#mainwrapper");
-    $mainwrapper.addClass('hide-for-small-only');
+    toggleMainwrapperOnSmallDevices(false);
   });
 
   $(document).on('close.fndtn.reveal', '[data-reveal]', function() {
-    var modal = document.getElementById('bibdk-modal');
-    Drupal.detachBehaviors(modal, null, null);
-
-    var $mainwrapper = $("#mainwrapper");
-    $mainwrapper.removeClass('hide-for-small-only');
+    toggleBehaviorsOnModal(false);
+    toggleMainwrapperOnSmallDevices(true);
   });
 
   $(document).on('closed.fndtn.reveal', '[data-reveal]', function() {
+    removeFilterOnMainwrapper();
+  });
+
+  function toggleMainwrapperOnSmallDevices(toggle){
+    var $mainwrapper = $("#mainwrapper");
+    if(toggle){
+      $mainwrapper.removeClass('hide-for-small-only');
+    } else {
+      $mainwrapper.addClass('hide-for-small-only');
+    }
+  }
+
+  function toggleBehaviorsOnModal(toggle){
+    var modal = document.getElementById('bibdk-modal');
+    if(toggle){
+      Drupal.attachBehaviors(modal, null);
+    } else {
+      Drupal.detachBehaviors(modal, null, null);
+    }
+  }
+
+  function addFilterOnMainwrapper(){
+    var $mainwrapper = $("#mainwrapper");
+    $mainwrapper.css("filter", "blur(20px)");
+    $mainwrapper.css("-webkit-filter", "blur(20px)");
+    $mainwrapper.css("-moz-filter", "blur(20px)");
+    $mainwrapper.css("-o-filter", "blur(20px)");
+    $mainwrapper.css("-ms-filter", "blur(20px)");
+  }
+
+  function removeFilterOnMainwrapper(){
     var $mainwrapper = $("#mainwrapper");
     $mainwrapper.css("filter", "");
     $mainwrapper.css("-webkit-filter", "");
     $mainwrapper.css("-moz-filter", "");
     $mainwrapper.css("-o-filter", "");
     $mainwrapper.css("-ms-filter", "");
-  });
+  }
 })();
