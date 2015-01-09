db1b816698c80cdbafc23e8e6f32125f7a82eee4
JrgenGNielsen <jgn@dbc.dk>
2015-01-09 11:41:03 +0100

    bug18167 help menu popup bugfix

--- js/lib/jquery.popupwindow.js
+++ js/lib/jquery.popupwindow.js
@@ -16,7 +16,7 @@ jQuery.fn.popupwindow = function(p, spawn) {
       height: 600, // sets the height in pixels of the window.
       width: 600, // sets the width in pixels of the window.
       toolbar: 0, // determines whether a toolbar (includes the forward and back buttons) is displayed {1 (YES) or 0 (NO)}.
-      scrollbars: 0, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
+      scrollbars: 1, // determines whether scrollbars appear on the window {1 (YES) or 0 (NO)}.
       status: 0, // whether a status line appears at the bottom of the window {1 (YES) or 0 (NO)}.
       resizable: 1, // whether the window can be resized {1 (YES) or 0 (NO)}. Can also be overloaded using resizable.
       left: 0, // left position when the window appears.
