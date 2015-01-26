cff128ee30d6684a305fa2b75e21ce67b660d551
Søren Vibjerg <vibjerg@gmail.com>
2015-01-26 14:22:54 +0100

    upgrade to jquery 2.1

--- template.php
+++ template.php
@@ -20,6 +20,11 @@ function bibdk_theme_css_alter(&$css) {
   unset($css['misc/vertical-tabs.css']);
 }
 
+function bibdk_theme_js_alter(&$js) {
+  $js['misc/jquery.js']['data'] = 'profiles/bibdk/themes/bibdk_theme/libs/foundation/js/vendor/jquery.js';
+  $js[drupal_get_path('theme', 'bibdk_theme') .'/libs/jquery-migrate/jquery-migrate-1.2.1.min.js']['group'] = -100;
+}
+
 /**
  * Implements hook_theme().
  */
