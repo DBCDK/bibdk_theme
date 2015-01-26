6ee4ab1c0cc16648abf617cf90ef43e90c0cbcf3
Søren Vibjerg <vibjerg@gmail.com>
2015-01-26 14:26:16 +0100

    fix autocomplete for mobile devices

--- js/bibdk/footer/autocomplete.js
+++ js/bibdk/footer/autocomplete.js
@@ -1,10 +1,64 @@
-(function() {
-  if(Modernizr && Modernizr.touch){
-    console.log('kanin');
-  }
+(function($){
 
-  $(document).on('touchend', '#autocomplete li', function (e) {
-    $(this).trigger('mousedown');
-    console.log(this);
-  });
-})();
+  /**
+   * overrides Drupal.jsAC.prototype.found
+   * @see misc/autocomplete.js
+   *
+   * Fixes autocomplete for touch devices using click instead of mousedown
+   */
+  Drupal.jsAC.prototype.found = function(matches){
+    console.log(matches);
+    // If no value in the textfield, do not show the popup.
+    if (!this.input.value.length){
+      return false;
+    }
+
+    // Prepare matches.
+    var ul = $('<ul></ul>');
+    var ac = this;
+    for (key in matches) {
+      $('<li></li>')
+        .html($('<div></div>').html(matches[key]))
+        .click(function(){
+          console.log(this);
+          ac.select(this);
+        })
+        .mouseover(function(){
+          ac.highlight(this);
+        })
+        .mouseout(function(){
+          ac.unhighlight(this);
+        })
+        .data('autocompleteValue', key)
+        .appendTo(ul);
+    }
+
+    // Show popup with matches, if any.
+    if (this.popup){
+      if (ul.children().length){
+        $(this.popup).empty().append(ul).show();
+        $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
+      }
+      else {
+        $(this.popup).css({ visibility: 'hidden' });
+        this.hidePopup();
+      }
+    }
+  };
+
+  /**
+   * overrides Drupal.autocompleteSubmit
+   * @see misc/autocomplete.js
+   *
+   * We want to submit on enter
+   */
+  Drupal.autocompleteSubmit = function(){
+    $('#autocomplete').each(function(){
+      this.owner.hidePopup();
+    });
+
+    return true;
+  };
+
+})(jQuery);
+
