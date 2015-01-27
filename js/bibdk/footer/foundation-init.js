90b80c7250776a0e4c1a229493d904f1eae78cd4
Søren Vibjerg <vibjerg@gmail.com>
2015-01-27 11:31:55 +0100

    fix init of foundation

--- js/bibdk/footer/foundation-init.js
+++ js/bibdk/footer/foundation-init.js
@@ -1,5 +1,5 @@
 (function ($) {
-  $(document).foundation({
+  $(window.document).foundation({
     offcanvas: {
       // Sets method in which offcanvas opens.
       // [ move | overlap_single | overlap ]
