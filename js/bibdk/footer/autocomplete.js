e74143029da819919f6ba5de57236e5bb3694b85
Søren Vibjerg <vibjerg@gmail.com>
2015-01-28 10:30:53 +0100

    check if object exists

--- js/bibdk/footer/autocomplete.js
+++ js/bibdk/footer/autocomplete.js
@@ -14,45 +14,45 @@
    *
    * Fixes autocomplete for touch devices using click instead of mousedown
    */
-  Drupal.jsAC.prototype.found = function(matches){
-    console.log(matches);
-    // If no value in the textfield, do not show the popup.
-    if (!this.input.value.length){
-      return false;
-    }
-
-    // Prepare matches.
-    var ul = $('<ul></ul>');
-    var ac = this;
-    for (key in matches) {
-      $('<li></li>')
-        .html($('<div></div>').html(matches[key]))
-        .click(function(){
-          ac.select(this);
-        })
-        .mouseover(function(){
-          ac.highlight(this);
-        })
-        .mouseout(function(){
-          ac.unhighlight(this);
-        })
-        .data('autocompleteValue', key)
-        .appendTo(ul);
-    }
-
-    // Show popup with matches, if any.
-    if (this.popup){
-      if (ul.children().length){
-        $(this.popup).empty().append(ul).show();
-        $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
+  if (Drupal.jsAC){
+    Drupal.jsAC.prototype.found = function(matches){
+      // If no value in the textfield, do not show the popup.
+      if (!this.input.value.length){
+        return false;
       }
-      else {
-        $(this.popup).css({ visibility: 'hidden' });
-        this.hidePopup();
-      }
-    }
-  };
 
+      // Prepare matches.
+      var ul = $('<ul></ul>');
+      var ac = this;
+      for (key in matches) {
+        $('<li></li>')
+          .html($('<div></div>').html(matches[key]))
+          .click(function(){
+            ac.select(this);
+          })
+          .mouseover(function(){
+            ac.highlight(this);
+          })
+          .mouseout(function(){
+            ac.unhighlight(this);
+          })
+          .data('autocompleteValue', key)
+          .appendTo(ul);
+      }
+
+      // Show popup with matches, if any.
+      if (this.popup){
+        if (ul.children().length){
+          $(this.popup).empty().append(ul).show();
+          $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
+        }
+        else {
+          $(this.popup).css({ visibility: 'hidden' });
+          this.hidePopup();
+        }
+      }
+    };
+  }
   /**
    * overrides Drupal.autocompleteSubmit
    * @see misc/autocomplete.js
