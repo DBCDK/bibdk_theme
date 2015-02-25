e9fff0b84f9ae488d1ad6fb1dc0ce397bc4698d3
JrgenGNielsen <jgn@dbc.dk>
2015-02-25 13:27:41 +0100

    update mobile facetbrowser css & js

--- js/bibdk/footer/mobile-facetbrowser.js
+++ js/bibdk/footer/mobile-facetbrowser.js
@@ -1,22 +1,19 @@
 /**
  * @file
- * Mobile off-canvas functionality for facets
+ * Mobile Foundation reveal functionality for facets
  */
+
 (function($){
+
   Drupal.behaviors.modalFacets = {
     attach: function(context, settings) {
       $('.bibdk-facets-mobile').click(function(e) {
-
-        e.preventDefault();
-
-        var facetbrowser_clone = $('.pane-bibdk-facetbrowser').clone();
-        
-        if ( !$('#bibdk-modal .pane-bibdk-facetbrowser').length ) {
-          $('#bibdk-modal').html(facetbrowser_clone);
-        }
-        
-        $('#bibdk-modal').foundation('reveal', 'open');
-
+        // e.preventDefault();
+        // var facetbrowser_clone = $('.pane-bibdk-facetbrowser').clone();
+        // if ( !$('#bibdk-modal .pane-bibdk-facetbrowser').length ) {
+        //   $('#bibdk-modal').html(facetbrowser_clone);
+        // }
+        // $('#bibdk-modal').foundation('reveal', 'open');
       });
     }
   };
