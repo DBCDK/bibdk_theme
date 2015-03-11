8bda1b913da36bdfee664602c0469eeef6990b6a
JrgenGNielsen <jgn@dbc.dk>
2015-03-11 16:52:03 +0100

    facets for mobile, css and js

--- js/bibdk/footer/mobile-facetbrowser.js
+++ js/bibdk/footer/mobile-facetbrowser.js
@@ -3,18 +3,37 @@
  * Mobile Foundation reveal functionality for facets
  */
 
-(function($){
+(function($) {
 
-  Drupal.behaviors.modalFacets = {
+  Drupal.behaviors.bibdkFacetMobile = {
     attach: function(context, settings) {
-      $('.bibdk-facets-mobile').click(function(e) {
-        // e.preventDefault();
-        // var facetbrowser_clone = $('.pane-bibdk-facetbrowser').clone();
-        // if ( !$('#bibdk-modal .pane-bibdk-facetbrowser').length ) {
-        //   $('#bibdk-modal').html(facetbrowser_clone);
-        // }
-        // $('#bibdk-modal').foundation('reveal', 'open');
+
+      // select facet group
+      var clickHandlerFacetGroup = function() {
+        $(this).find('input').click();
+      };
+
+      $('.bibdk-facetbrowser-mobile.facet-group .facet-wrapper').each(function(event) {
+        $(this).not('.processed').on("click", clickHandlerFacetGroup);
+        $(this).addClass('processed'); // click event is attached twice(!?) otherwise
       });
+
+      // check/uncheck facet values
+      var clickHandlerFacetValues = function() {
+        if ( $(this).find('.facet-select-actions input').prop('value') == '1' ) {
+          $(this).find('.facet-select-actions input').prop('value', '0');
+        } else {
+          $(this).find('.facet-select-actions input').prop('value', '1');
+        }
+        $(this).find('.facet-checkmark-wrapper span').toggleClass("checked");
+      };
+
+      $('.bibdk-facetbrowser-mobile.facet-values .facet-wrapper').each(function(event) {
+        $(this).not('.processed').on("click", clickHandlerFacetValues);
+        $(this).addClass('processed'); // click event is attached twice(!?) otherwise
+      });
+
     }
   };
+
 })(jQuery);
