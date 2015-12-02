dd37877241ea198c548f1426f8facdf0ba46af48
pjohans <pjohans@gmail.com>
2015-12-02 15:45:17 +0100

    remove dpm

--- templates/elements/bibdk-icon.tpl.php
+++ templates/elements/bibdk-icon.tpl.php
@@ -1,4 +1,3 @@
-<?php dpm($icon, 'ICON'); ?>
 <span class="svg-icon">
   <svg class="<?php print $icon; ?>" title="<?php print $title;?>" alt="<?php print $title;?>">
     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-<?php print $icon; ?>"></use>
