7a54dc5d0f2466e84a6f9c694dd68f3ac0dd769c
Anders Andersen <ana@dscrum-i01.dbc.dk>
2015-12-04 13:20:36 +0100

    check for windows phone

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns="http://www.w3.org/1999/html">
   <head>
-    <!-- Windows phone hack - IE 11 do not all ways work on click on dropdownbox -->
-    <meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">
+    <!-- Windows phone hack - IE 11 do not allways work on click on dropdownbox -->
+    <?php
+       $wp = $_SERVER['HTTP_USER_AGENT'];
+       preg_match('/IEMobile\/\d{1,2}.\d{1,2}/', $wp, $matches);
+       if (!empty($matches)) {
+         if (strcmp($matches[0], 'IEMobile/11.0') == 0) {
+           print '<meta http-equiv="X-UA-Compatible" content="IE=10,chrome=1">';
+         }
+       }
+    ?>
     <meta name="viewport" content="initial-scale=1">
     <?php print $head; ?>
     <title><?php print $head_title; ?></title>
