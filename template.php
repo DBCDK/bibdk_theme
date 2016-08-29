c7ee295d932abc8e60d03886cf729ef44d08dba9
pjohans <pjohans@gmail.com>
2016-08-29 11:37:24 +0200

    remove references to nonexisting modules

--- template.php
+++ template.php
@@ -13,8 +13,6 @@ function bibdk_theme_css_alter(&$css) {
   unset($css[drupal_get_path('module', 'system') . '/system.theme.css']);
   unset($css[drupal_get_path('module', 'system') . '/system.menus.css']);
   unset($css[drupal_get_path('module', 'user') . '/user.css']);
-  unset($css[drupal_get_path('module', 'ding_facetbrowser') . '/css/facetbrowser.css']);
-  unset($css[drupal_get_path('module', 'ting_search_carousel') . '/css/ting_search_carousel.css']);
   unset($css[drupal_get_path('module', 'user_alert') . '/css/user-alert.css']);
   unset($css[drupal_get_path('module', 'bibdk_help') . '/css/bibdk_help.css']);
   unset($css[drupal_get_path('module', 'ctools') . '/css/modal.css']);
