294e495f1818f508a2555bc5b08922b96dc336d0
Lars Hansen <lkh@dbc.dk>
2019-11-21 11:24:15 +0100

    First go at new front page for bibliotek.dk

--- templates/blocks/search-block-form.tpl.php
+++ templates/blocks/search-block-form.tpl.php
@@ -1,30 +1,31 @@
-<!-- SEARCH PAGE NAVIGATION -->
+<!-- SEARCH PAGE NAVIGATION --
 <nav id="custom-search--navigation" class="show-for-large-up">
   <div class="row">
     <div class="medium-24 columns">
-      <?php print $search['searchpages']; ?>
+      <?php //print $search['searchpages']; ?>
     </div>
   </div>
 </nav>
-<!-- SEARCH PAGE NAVIGATION END-->
+!-- SEARCH PAGE NAVIGATION END-->
+<section>
+  <div class="row">
+    <div class="medium-24 columns">
+      <span id="seasonal"></span>
 
-<div class="row">
-  <div class="medium-24 columns">
-    <span id="seasonal"></span>
+      <div id="search-block-wrapper" class="clearfix">
+        <div id="search-block-primary">
+          <?php print $search['search_block_form']; ?>
+          <?php print $search['select_material_type']; ?>
+          <?php print $search['actions']; ?>
+        </div>
 
-    <div id="search-block-wrapper" class="clearfix">
-      <div id="search-block-primary">
-        <?php print $search['search_block_form']; ?>
-        <?php print $search['select_material_type']; ?>
-        <?php print $search['actions']; ?>
+        <?php if (isset($search['advanced'])): ?>
+          <?php //print $search['advanced']; ?>
+        <?php endif; ?>
+
       </div>
 
-      <?php if (isset($search['advanced'])): ?>
-        <?php print $search['advanced']; ?>
-      <?php endif; ?>
-
+      <?php print $search['hidden']; ?>
     </div>
-
-    <?php print $search['hidden']; ?>
   </div>
-</div>
+</section>
