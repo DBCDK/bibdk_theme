a764c2c1b1a1bd497ad7103cfaf2414585e9e089
Lars Hansen <lkh@dbc.dk>
2019-11-22 13:50:44 +0100

    second attempt at the new frontpage

--- templates/blocks/search-block-form.tpl.php
+++ templates/blocks/search-block-form.tpl.php
@@ -1,31 +1,29 @@
-<!-- SEARCH PAGE NAVIGATION --
-<nav id="custom-search--navigation" class="show-for-large-up">
-  <div class="row">
-    <div class="medium-24 columns">
-      <?php //print $search['searchpages']; ?>
-    </div>
-  </div>
-</nav>
-!-- SEARCH PAGE NAVIGATION END-->
-<section>
-  <div class="row">
-    <div class="medium-24 columns">
-      <span id="seasonal"></span>
+<div class="row">
+  <div class="medium-24 columns">
+    <span id="seasonal"></span>
 
-      <div id="search-block-wrapper" class="clearfix">
-        <div id="search-block-primary">
-          <?php print $search['search_block_form']; ?>
-          <?php print $search['select_material_type']; ?>
-          <?php print $search['actions']; ?>
-        </div>
-
-        <?php if (isset($search['advanced'])): ?>
-          <?php //print $search['advanced']; ?>
-        <?php endif; ?>
-
+    <div id="search-block-wrapper" class="clearfix">
+      <h1>Søg, find og lån fra alle Danmarks biblioteker</h1>
+      <div id="search-block-primary">
+        <?php print $search['search_block_form']; ?>
+        <?php print $search['select_material_type']; ?>
+        <?php print $search['actions']; ?>
       </div>
+      <!-- SEARCH PAGE NAVIGATION -->
+      <nav id="custom-search--navigation" class="show-for-large-up">
+        <div class="row">
+          <div class="medium-24 columns">
+            <?php print $search['searchpages']; ?>
+          </div>
+        </div>
+      </nav>
+      <!-- SEARCH PAGE NAVIGATION END-->
+      <?php if (isset($search['advanced'])): ?>
+        <?php //print $search['advanced']; ?>
+      <?php endif; ?>
 
-      <?php print $search['hidden']; ?>
     </div>
+
+    <?php print $search['hidden']; ?>
   </div>
-</section>
+</div>
