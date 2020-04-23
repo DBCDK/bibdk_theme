24d53f1dbee7a888f881a9662a49c13a98f75c7e
Rikke Lose <rikkelose@172-19-2-100.dhcpaddr.dbc.dk>
2020-04-23 15:52:02 +0200

    make image file permanent

--- theme-settings.php
+++ theme-settings.php
@@ -1,6 +1,14 @@
 <?php
 
 function bibdk_theme_form_system_theme_settings_alter(&$form, $form_state) {
+
+  $bg_image = theme_get_setting('background_image');
+  $settings_theme = $form_state['build_info']['args'][0];
+  // BUG: Force file to be permanent.
+  if (!empty($bg_image)) {
+    _fix_permanent_image('background_image', $settings_theme);
+  }
+
   $form['background'] = array(
     '#type' => 'fieldset',
     '#title' => t('Background Image Settings', array(), array('options' => array('context' => 'bibdk_theme'))),
@@ -24,4 +32,18 @@ function bibdk_theme_form_system_theme_settings_alter(&$form, $form_state) {
     "#title" => t('Image title', array(), array('options' => array('context' => 'bibdk_theme'))),
     '#default_value' => theme_get_setting('background_image_title'),
   );
+}
+
+function _fix_permanent_image($image, $theme) {
+  $fid = theme_get_setting($image, $theme);
+  if ($fid > 0) {
+    $file = file_load($fid);
+    if (is_object($file) && $file->status == 0) {
+      $file->status = FILE_STATUS_PERMANENT;
+      file_save($file);
+      file_usage_add($file, $theme, 'theme', 1);
+      $message = t($image . 'saved permanently.', array(), array('options' => array('context' => 'bibdk_theme')));
+      drupal_set_message($massage, 'status');
+    }
+  }
 }
