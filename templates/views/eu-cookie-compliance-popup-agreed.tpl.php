c44a48b0577ce541066fa35451af99a4947cb29b
Jørgen G. Nielsen <jgn@dbc.dk>
2020-04-29 10:32:59 +0200

    move tracking scripts to bibdk_frontend module, and theme eu-cookie-compliance

--- templates/views/eu-cookie-compliance-popup-agreed.tpl.php
+++ templates/views/eu-cookie-compliance-popup-agreed.tpl.php
@@ -1,8 +1,8 @@
 <?php
+
 /**
  * @file
- * This is a template file for a pop-up informing a user that he has already
- * agreed to cookies.
+ * Template file for a banner informing a user that he has agreed to cookies.
  *
  * When overriding this template it is important to note that jQuery will use
  * the following classes to assign actions to buttons:
@@ -11,18 +11,21 @@
  * find-more-button  - link to an information page
  *
  * Variables available:
- * - $message:  Contains the text that will be display whithin the pop-up
+ * - $message:  Contains the text that will be display within the pop-up
+ * - $hide_button: Contains hide button title
+ * - $find_more_button: Contains find more button title
  */
 ?>
-
 <div>
-  <div class ="popup-content agreed">
+  <div class="popup-content agreed">
     <div id="popup-text">
-      <?php print t( "messagetekst"); ?>
+      <?php print t($message, array(), array('context' => 'eu-cookie-compliance')) ?>
     </div>
     <div id="popup-buttons">
-      <button type="button" class="hide-popup-button"><?php print t("Hide this message"); ?> </button>
-      <button type="button" class="find-more-button" ><?php print t("More information on cookies"); ?></button>
+      <button type="button" class="hide-popup-button eu-cookie-compliance-hide-button"><?php print t($hide_button, array(), array('context' => 'eu-cookie-compliance')); ?></button>
+      <?php if ($find_more_button) : ?>
+        <button type="button" class="find-more-button eu-cookie-compliance-more-button-thank-you" ><?php print t($find_more_button, array(), array('context' => 'eu-cookie-compliance')); ?></button>
+      <?php endif; ?>
     </div>
   </div>
 </div>
